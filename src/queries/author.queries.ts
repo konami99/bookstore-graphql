a94f51bc682a4dc2505b746ce6217d7dda664f6b
Richard Chou <konami99@hotmail.com>
2023-10-05 21:43:52 +1100

    updated getAuthor

--- src/queries/author.queries.ts
+++ src/queries/author.queries.ts
@@ -2,20 +2,24 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient()
 
-export function getAuthor(id: any): any {
+export async function getAuthor(id: string): Promise<any> {
   try {
-    const resp = {
-      id: 1,
-      name: 'Rex',
-      gender: 'M'
-    }
-    return resp
+    return prisma.author.findFirst({
+      where: {
+        id: parseInt(id)
+      },
+      include: {
+        pseudonym: true,
+        bankAccounts: true,
+        books: true
+      }
+    })
   } catch (err) {
     return err
   }
 }
 
-export function listAuthors(): any {
+export async function listAuthors(): Promise<any> {
   try {
     return prisma.author.findMany({
       include: {
@@ -23,7 +27,7 @@ export function listAuthors(): any {
         bankAccounts: true,
         books: true
       }
-  });
+    });
   } catch (err) {
     return err
   }
